447dd629e5653e67c93adf0ad372ff4a71a939c0
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-04-11 14:40:10 +0200

    Frontcover added

--- js/zadanie_STXNext.js
+++ js/zadanie_STXNext.js
@@ -19,19 +19,45 @@ $(() => {
             data: {
                 q: searchQuery,
             }
-        }).done((response) => {
-            console.log(response);
-            // bookList.empty();
-            for (var i = 0; i < response.items.length; i++) {
-                // var bookPicture = $("<img src=" + response.items[i].volumeInfo.imageLinks + "/>");
-                var bookTitle = $("<li>" + response.items[i].volumeInfo.title + "</li>");
-                var bookDescription = $("<p>" + response.items[i].volumeInfo.description + "</p>");
+        }).done((data) => {
+            if(data.totalItems > 0 ) {
+                console.log(data);
+                // bookList.empty();
 
-                // console.log(response.items[i].volumeInfo.title);
-                console.log(bookTitle);
-                // bookList.append(bookPicture);
-                bookList.append(bookTitle);
-                bookList.append(bookDescription);
+                for (var i = 0; i < data.items.length; i++) {
+
+                    var imgCheck = data.items[i].volumeInfo.imageLinks;
+                    var descCheck = data.items[i].volumeInfo.description;
+                    var snippetCheck = data.items[i].searchInfo;
+                    var imgSrc, description;
+
+                    if (imgCheck !== undefined) {
+                        imgSrc = data.items[i].volumeInfo.imageLinks.thumbnail;
+                    } else {
+                        imgSrc = 'http://placehold.it/128x198';
+                    }
+
+                    if (descCheck !== undefined) {
+                        description = descCheck;
+                    } else if (snippetCheck !== undefined) {
+                        description = snippetCheck.textSnippet;
+                    } else {
+                        description = 'Unfortunately, there\'s no more information for you';
+                    }
+
+                    var bookCover = $("<img src=" + imgSrc + "/>");
+                    var bookTitle = $("<li>" + (i + 1) + "." + " " + data.items[i].volumeInfo.title + "</li>");
+                    var bookDescription = $("<p>" + description + "</p>");
+                    var bookListItem = $("<div>" + "</div>");
+
+                    bookCover.addClass("bookcover_img");
+                    bookListItem.addClass("bookListItem_ctn");
+                    bookDescription.addClass("book_description");
+                    bookList.append(bookListItem);
+                    bookListItem.append(bookTitle);
+                    bookListItem.append(bookCover);
+                    bookListItem.append(bookDescription);
+                }
             }
         }).fail((error) => {
             console.log(error);
