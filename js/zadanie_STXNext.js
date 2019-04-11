5c62fdf5b3e464e8b8d55eae49b95b4527e8f154
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-04-11 15:04:39 +0200

    Css changed

--- js/zadanie_STXNext.js
+++ js/zadanie_STXNext.js
@@ -20,9 +20,9 @@ $(() => {
                 q: searchQuery,
             }
         }).done((data) => {
-            if(data.totalItems > 0 ) {
+            if (data.totalItems > 0) {
                 console.log(data);
-                // bookList.empty();
+                bookList.empty();
 
                 for (var i = 0; i < data.items.length; i++) {
 
@@ -65,53 +65,3 @@ $(() => {
     };
 
 });
-
-// $(function () {
-//     var apiUrl = "https://pixabay.com/api/";
-//     var apiKey = "11792782-6296e248ec152f11c55ec463e";
-//     var form = $(".form");
-//     var input = $(".form-search");
-//     var gallery = $(".gallery");
-//     var submit = form.find("button");
-//
-//     form.on("submit", function (event) {
-//         event.preventDefault();
-//         submit.attr("disabled", true);
-//         submit.addClass("loading");
-//         var searchQuery = input.val();
-//         loadImages(searchQuery);
-//     });
-//
-//
-//     function loadImages(searchQuery) {
-//         $.ajax({
-//             url: apiUrl,
-//             method: "GET",
-//             data: {
-//                 q: searchQuery,
-//                 key: apiKey //do repozytorium jak wrzuce to usunac klucz i napisac zeby swoj ktos wstawil//
-//             }
-//         }).done(function (response) {
-//             console.log(response);
-//             gallery.empty();
-//             gallery.addClass("loading");
-//             for (var i = 0; i < response.hits.length; i++) {
-//                 var link = $("<a href=" + response.hits[i].largeImageURL + "></a>");
-//                 link.attr("data-fancybox", "gallery");
-//                 var picture = $("<img src=" + response.hits[i].largeImageURL + "/>");
-//                 link.append(picture);
-//                 picture.css({
-//                     "height": "150px",
-//                     "width": "150px",
-//                     "margin": "3px",
-//                 });
-//                 gallery.append(link);
-//                 submit.attr("disabled", false);
-//                 submit.removeClass("loading");
-//                 gallery.removeClass("loading");
-//             }
-//         }).fail(function (error) {
-//             console.log(error);
-//         })
-//     }
-// });
